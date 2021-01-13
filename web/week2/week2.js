360578036e774239d65f44860aaa36589b549263
melissaportfolio <ellieelise@live.com>
2021-01-12 20:50:33 -0700

    added js for color change

--- web/week2/week2.js
+++ web/week2/week2.js
@@ -4,6 +4,13 @@ function clickAlert() {
 
 function changeColor() {
     /* Get text from textbox */
-    var textboxId = textColor;
+    var textId = "txtColor";
+	var textbox = document.getElementById(textId);
     /* Set color of first div */
+    var divId = "div-1";
+	var div = document.getElementById(divId);
+
+	//verify values here 
+	var color = textbox.value;
+	div.style.backgroundColor = color;
 }
