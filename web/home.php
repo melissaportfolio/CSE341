a22c3f5c256654c7a15ba7c0fa367b27aa42146f
melissaportfolio <ellieelise@live.com>
2021-01-13 18:43:59 -0700

    added files

---
+++ web/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSE341 | Melissa Hall</title>
+</head>
+<body>
+    <footer>
+        <?php require $_SERVER['DOCUMENT_ROOT'] . '/partials/footer.php.php'; ?>
+    </footer>
+</body>
+</html>
