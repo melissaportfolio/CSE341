7345b3f3845b634a9bec09920b53a18b0e2c843c
melissaportfolio <ellieelise@live.com>
2021-01-14 18:43:59 -0700

    updated css

--- web/partials/footer.php
+++ web/partials/footer.php
@@ -1,3 +1,3 @@
 <hr>
-<p>&copy; Melissa Hall | CSE341 | BYU Idaho</p>
+<p class="footer-text">&copy; Melissa Hall | CSE341 | BYU Idaho</p>
 <p><?php echo "Last Updated: " . date ("d F Y H:i:s", getlastmod());?></p>
