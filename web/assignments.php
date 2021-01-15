9638bb61fa6f2bdee17e5b17282081a8934e8ab6
melissaportfolio <ellieelise@live.com>
2021-01-14 18:48:41 -0700

    updated assignments page

--- web/assignments.php
+++ web/assignments.php
@@ -2,9 +2,13 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CSE341 | Melissa Hall</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Homemade+Apple&family=Merriweather&family=Playfair+Display&family=Quicksand&family=Roboto&family=Tangerine&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
@@ -16,7 +20,7 @@
 
 <body>
     <h1>Melissa Hall</h1>
-    <nav>
+    <nav class="navbar navbar-default">
         <?php require $_SERVER['DOCUMENT_ROOT'] . '/partials/nav.php'; ?>
     </nav>
     <h1>Coming Soon</h1>
--- web/home.php
+++ web/home.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CSE341 | Melissa Hall</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Homemade+Apple&family=Merriweather&family=Playfair+Display&family=Quicksand&family=Roboto&family=Tangerine&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Homemade+Apple&family=Merriweather&family=Playfair+Display&family=Quicksand&family=Roboto&family=Tangerine&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
@@ -18,16 +20,16 @@
 
 <body>
     <h1>Melissa Hall</h1>
-    <nav>
+    <nav class="navbar navbar-default">
         <?php require $_SERVER['DOCUMENT_ROOT'] . '/partials/nav.php'; ?>
     </nav>
     <div>
-    <figure>
+        <figure>
             <figcaption>
                 <img src="images/NeboLoop.JPG" alt="image of mountain path">
                 <p class="fig-quote">Nature does not hurry, yet everything is accomplished.</p>
                 <p class="fig-author">Lao Tzu</p>
-                
+
             </figcaption>
         </figure>
     </div>
