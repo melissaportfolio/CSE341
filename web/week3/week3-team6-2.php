e7e68a58e062de1401a0000d87b8d17acf994a2b
melissaportfolio <ellieelise@live.com>
2021-01-19 21:41:37 -0700

    .

---
+++ web/week3/week3-team6-2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Welcome <?php echo $_POST["name"];?>
+    Your email address is <?php echo $_POST["email"];?>
+    Your major is <?php echo $_POST["major"];?>
+    Your comment is <?php echo $_POST["comment"];?>
+</body>
+</html>
