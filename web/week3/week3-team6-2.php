1c0a2ee7f17da353a295bf8ae1026e9a4f3aaf5b
melissaportfolio <ellieelise@live.com>
2021-01-19 21:47:27 -0700

    .

--- web/week3/week3-team6-2.php
+++ web/week3/week3-team6-2.php
@@ -10,5 +10,27 @@
     Your email address is <?php echo $_POST["email"];?>
     Your major is <?php echo $_POST["major"];?>
     Your comment is <?php echo $_POST["comment"];?>
+
+    Your visited continents are <?php if (isset($_POST["continent1"])){
+        echo $_POST["continent1"];
+    }?>
+    <?php if (isset($_POST["continent2"])){
+        echo $_POST["continent2"];
+    }?>
+    <?php if (isset($_POST["continent3"])){
+        echo $_POST["continent3"];
+    }?>
+    <?php if (isset($_POST["continent4"])){
+        echo $_POST["continent4"];
+    }?>
+    <?php if (isset($_POST["continent5"])){
+        echo $_POST["continent5"];
+    }?>
+    <?php if (isset($_POST["continent6"])){
+        echo $_POST["continent6"];
+    }?>
+    <?php if (isset($_POST["continent7"])){
+        echo $_POST["continent7"];
+    }?>
 </body>
 </html>
