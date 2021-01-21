3ece1139b3b4dae7daf43a0377ac66073d9db7cb
melissaportfolio <ellieelise@live.com>
2021-01-20 18:46:25 -0700

    .

--- web/view/browse-items.php
+++ web/view/browse-items.php
@@ -1,4 +1,7 @@
-<!DOCTYPE html>
+<?php session_start();
+require_once("checkout.php");
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -12,7 +15,21 @@
     </nav>
     <br><br><br>
 <h2>Purchase a therapy session:</h2>
-
+<form action="" method="post">
+<p>One hour individual session:</p>
+<p>One hour family session:</p>
+<p>Bundle of 10 individual sessions:</p>
+<p>Bundle of 10 family sessions:</p>
+<input id="session_ind" name="therapy" type="radio" value="therapy">
+<label for="session_ind">One hour individual session:</label>
+<input id="session_fam" name="therapy" type="radio" value="therapy">
+<label for="session_fam">One hour family session:</label>
+<input id="session_ind_10" name="therapy" type="radio" value="therapy">
+<label for="session_ind_10">Bundle of 10 individual sessions:</label>
+<input id="session_fam_10" name="therapy" type="radio" value="therapy">
+<label for="session_fam_10">Bundle of 10 family sessions:</label>
+<button type="submit" name="submit" value="true">Checkout</button>
+</form>
     <footer>
         <?php require $_SERVER['DOCUMENT_ROOT'] . '../partials/footer.php'; ?>
     </footer>
@@ -22,4 +39,8 @@
     </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </body>
-</html>
+</html><?php 
+if (isset($_GET["destroy"])){
+    session_destroy();
+}
+?>
