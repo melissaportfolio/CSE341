4585f9de949b9ae74874f7026a5ff692af27457c
melissaportfolio <ellieelise@live.com>
2021-01-20 21:29:03 -0700

    .

--- web/view/shoppingcart.php
+++ web/view/shoppingcart.php
@@ -16,7 +16,10 @@
 Name: <input type="text" name="name"><br>
 Email: <input type="email" name="email"><br>
 Mailing Address: <input type="text" name="address"><br>
-Order summary: <p><?php echo $_SESSION["therapy1"];?></p>
+Order summary: <?php foreach ($_SESSION as $key => $value)
+{
+    echo "<p>" . $value . "</p>";
+}?>
 
 
 </form>
