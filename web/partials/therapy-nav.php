1855f255de5ce36b5a4279baeff3a36f1fe9e70e
melissaportfolio <ellieelise@live.com>
2021-01-20 19:11:34 -0700

    updated nav and footer

--- web/partials/therapy-nav.php
+++ web/partials/therapy-nav.php
@@ -1,4 +1,4 @@
 <ul>  
-<li><a href="/home.php" class="nav">Home</a></li>
-<li><a href="/assignments.php" class="nav">Assignments</a></li>
+<li><a href="../view/index.php" class="nav">Home</a></li>
+<li><a href="../view/browse-items.php" class="nav">Schedule an appointment</a></li>
 </ul>
--- web/view/browse-items.php
+++ web/view/browse-items.php
@@ -1,5 +1,6 @@
 <?php session_start();
 require_once("checkout.php");
+require_once("confirmation.php")
 
 ?><!DOCTYPE html>
 <html lang="en">
