e9a06041deaaaee67e799f1f775add9a53710a3c
melissaportfolio <ellieelise@live.com>
2021-01-22 20:01:23 -0700

    updated nav and css

--- web/partials/therapy-nav.php
+++ web/partials/therapy-nav.php
@@ -1,4 +1,4 @@
 <ul>  
 <li><a href="../view/index.php" class="nav">Home</a></li>
-<li><a href="../view/browse-items.php" class="nav">Schedule an appointment</a></li>
+<li><a href="../view/browse-items.php" class="nav">Therapy Options</a></li>
 </ul>
