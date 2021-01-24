2b441d460c074dbe4739cf93dc81783492459307
melissaportfolio <ellieelise@live.com>
2021-01-23 19:08:45 -0700

    .

--- web/view/view-cart.php
+++ web/view/view-cart.php
@@ -45,7 +45,7 @@ Mailing Address: <input type="text" name="address"><br>
 -->
 <p>Order summary:</p> <?php foreach ($_SESSION as $key => $value)
 {
-    str_replace($value,"-"," ");
+    str_replace($value,"_"," ");
     echo "<p>" . $value . "</p>" .
     "<form action='' method='post'>" 
     . "<button  type='submit' name='removeItem" . $value . "' value='true'>Remove</button>"
