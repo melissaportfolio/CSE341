b643e043ee32f50e71d17c697ff91c4831476e51
melissaportfolio <ellieelise@live.com>
2021-02-02 21:37:21 -0700

    .

---
+++ db/week5.sql
@@ -0,0 +1,8 @@
+create table Scriptures (
+    id INT, 
+    book VARCHAR(30), 
+    chapter VARCHAR(30), 
+    verse VARCHAR(255), 
+    content VARCHAR(255));
+
+    INSERT INTO Scriptures
---
+++ web/week5/week5.php
@@ -0,0 +1,40 @@
+<?php
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+foreach ($db->query('SELECT book, chapter, verse, content FROM Scriptures') as $row)
+{
+  $scripture .= $row['book'] . ' '. $row['chapter'] . ' '. $row['verse'] . ' - ' . $row['content'];
+
+}
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 5 Team Assignment</title>
+</head>
+<body>
+    <h1>Scripture Resources</h1>
+<?=$scripture?>
+</body>
+</html>
