961d4cfbae71a544e87f0fc0416c725a3527f95e
melissaportfolio <ellieelise@live.com>
2021-02-02 22:29:42 -0700

    .

--- web/week5/details.php
+++ web/week5/details.php
@@ -1,4 +1,38 @@
-<!DOCTYPE html>
+<?php
+
+
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+echo $id;
+exit;
+    // $where = "SELECT id, book, chapter, verse, content FROM Scriptures WHERE book = '".$_POST['search']."'";
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+foreach ($db->query($where) as $row)
+{
+  $scripture .= '<a href="details.php?id='.$row['id'].'">' . $row['book'] . ':' . ' '. $row['chapter'] . ' '. $row['verse'] . '</a><br>';
+
+}?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
