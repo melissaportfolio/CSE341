face3f41736ebfaebfa5eacf5997a2a0c7372e4f
melissaportfolio <ellieelise@live.com>
2021-02-04 20:41:35 -0700

    .

--- web/view/admin-patient-details.php
+++ web/view/admin-patient-details.php
@@ -30,7 +30,7 @@ catch (PDOException $ex)
 
 foreach ($db->query($where) as $row)
 {
-    var_dump($row);
+    //var_dump($row);
     $patient .=  $row['first_name'] . ' '. $row['last_name'] . '<br>'. $row['street'] . ', ' . $row['city'] . ' ' . $row['state'] . ' ' . $row['zip'] . '<br>';
 
 }?><!DOCTYPE html>
