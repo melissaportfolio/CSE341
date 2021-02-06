c109cd8c7763143866a60dbbf55f40cfa9f228a9
melissaportfolio <ellieelise@live.com>
2021-02-06 15:38:31 -0700

    .

--- web/js/quotes.js
+++ web/js/quotes.js
@@ -1,5 +1,5 @@
 var d = new Date();
 var currentday1 = d.getDay();
-if (currentday1 == 7) {
+if (currentday1 == 6) {
     document.getElementsByClassName("quote1")[0].classList.toggle("banner1");
 }
