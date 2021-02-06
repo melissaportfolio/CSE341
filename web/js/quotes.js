cbd49dff91a2472413064e5ead421326bcf13297
melissaportfolio <ellieelise@live.com>
2021-02-06 15:46:30 -0700

    .

--- web/js/quotes.js
+++ web/js/quotes.js
@@ -1,5 +1,30 @@
+// var d = new Date();
+// var currentday1 = d.getDay();
+// if (currentday1 == 1) {
+//     document.getElementsByClassName("quote1")[0].classList.toggle("banner1");
+// }
+
 var d = new Date();
-var currentday1 = d.getDay();
-if (currentday1 == 6) {
-    document.getElementsByClassName("quote1")[0].classList.toggle("banner1");
-}
+var currentday = d.getDay();
+switch (currentday) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = document.getElementsByClassName("quote1")[0].classList.toggle("banner1");
+  }
