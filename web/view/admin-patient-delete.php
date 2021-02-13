a5debb17ff04b8f6178ce243bf96cb3caaf2c23a
melissaportfolio <ellieelise@live.com>
2021-02-12 20:31:14 -0700

    .

--- web/view/admin-patient-delete.php
+++ web/view/admin-patient-delete.php
@@ -6,13 +6,13 @@ $customer_id = $_POST['customer_id'];
 echo 'Hello' . $customer_id;
 $customer_id = filter_input(INPUT_GET, 'customer_id', FILTER_VALIDATE_INT);
 
-$where1 = "DELETE FROM journal WHERE customer_id = :customer_id";
+$where1 = "DELETE FROM journal WHERE customer_id = $customer_id";
 $stmt = $db->prepare($where1);
 $stmt->bindValue(':customer_id', $customer_id, PDO::PARAM_INT);
 $stmt->execute();
 
 
-$where2 = "DELETE FROM customer WHERE customer_id = :customer_id";
+$where2 = "DELETE FROM customer WHERE customer_id = $customer_id";
 $stmt = $db->prepare($where2);
 $stmt->bindValue(':customer_id', $customer_id, PDO::PARAM_INT);
 $stmt->execute();
